feca470118a5f602bb91217ddfde39d86fe6a552
litisha7 <157836349+litisha7@users.noreply.github.com>
2024-02-23 13:11:59 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/card_quiz_game_yadavl1/GameRules.java
@@ -0,0 +1,38 @@
+package com.example.card_quiz_game_yadavl1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/************************************************************************************
+ *
+ *  The GameRules activity captures displays the rules of the Card Quiz Game
+ * @author Pallavi Tilloo
+ *
+ *************************************************************************************/
+
+public class GameRules extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_rules);
+        getSupportActionBar().hide();
+    }
+
+    public void startQuiz(View view) {
+        //If the user clicks on Start
+        Intent i = new Intent(this, Quiz.class);
+        startActivity(i);
+    }
+
+    public void ViewStats(View view){
+        //If the user clicks on View Previous Scores
+        Intent i = new Intent(this, ScoreStats.class);
+        startActivity(i);
+    }
+}
+
+/************************************** End of GameRules Class ********************************/
